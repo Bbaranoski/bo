5377ce24d959a860fdc5e53d71cfb57c1e150d01
Bbaranoski <breno.b.o.baranoski@gmail.com>
2024-04-13 08:08:50 -0300

    consertando a limitacao do movimento do botao

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+const body = document.body
 const paia = document.getElementById('paia')
 paia.addEventListener('mouseover', () => {
     paia.style.position = 'absolute';
-    var x = Math.floor(Math.random()*500) + 1;
-    var y = Math.floor(Math.random()*500) + 1;
+    var x = Math.floor(Math.random()*body.offsetWidth - 50) + 1;
+    var y = Math.floor(Math.random()*body.offsetHeight - 50) + 1;
     paia.style.left = x + 'px';
     paia.style.top = y + 'px';
-    console.log(x)
+    console.log(body.offsetHeight)
 })
